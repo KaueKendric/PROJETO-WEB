a8dce22ae9e26dec40e2c51d5fbee58298424b49
KaueKendric <kauekendric270@gmail.com>
2025-06-04 12:37:45 -0300

    configurando frontend

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
-from app.routers import cadastro, agendamento, funcionario 
+from fastapi.middleware.cors import CORSMiddleware
+from app.routers import cadastro, agendamento, funcionario
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(cadastro.router)
 app.include_router(agendamento.router)
---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ListaCadastros from './components/ListaCadastros';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PROJETO WEB</h1>
+        <ListaCadastros /> {}
+      </header>
+    </div>
+  );
+}
+
+export default App;
